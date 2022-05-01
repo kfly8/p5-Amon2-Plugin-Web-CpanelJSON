d9b7f6d960e75f2f1d0fd9fbb3961b6ceb2baa6e
kfly8 <kfly@cpan.org>
2022-05-01 20:59:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::Plugin::Web::CpanelJSON - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::Plugin::Web::CpanelJSON;
+
+# DESCRIPTION
+
+Amon2::Plugin::Web::CpanelJSON is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly@cpan.org>
---
+++ lib/Amon2/Plugin/Web/CpanelJSON.pm
@@ -0,0 +1,39 @@
+package Amon2::Plugin::Web::CpanelJSON;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::Plugin::Web::CpanelJSON - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::Plugin::Web::CpanelJSON;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Web::CpanelJSON is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly@cpan.orgE<gt>
+
+=cut
+
